8e466b223c0e7476843cb9eecaaf634ad6279f34
HH <hurricane@kotukusystems.com>
2020-03-30 13:57:27 +1300

    Change to prismic {{}}

--- components/slices/BasicCardsSlice.vue
+++ components/slices/BasicCardsSlice.vue
@@ -1,36 +1,32 @@
 <template>
-    <section class="bg-primary-alt">
-      <div class="container">
-        <div class="row justify-content-center">
-          <div class="col">
-            <div class="row">
-              <div class="col-md-6 col-lg-4"  v-for="item in slice.items" :key="item.id">
-                <div class="card">
+  <section class="bg-primary-alt">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col">
+          <div class="row">
+            <div class="col-md-6 col-lg-4" v-for="item in slice.items" :key="item.id">
+              <div class="card">
+                <a :href="item.card_link">
+                  <img :src="item.card_image.url" alt="Image" class="card-img-top" />
+                </a>
+                <div class="card-body d-flex flex-column">
                   <a :href="item.card_link">
-                    <img :src="item.card_image.url" alt="Image" class="card-img-top">
+                    <h4>{{$prismic.richTextAsPlain(item.card_title)}}</h4>
                   </a>
-                  <div class="card-body d-flex flex-column">
-                    <a :href="item.card_link">
-                      <h4><prismic-rich-text  :field="item.card_title"/></h4>
-                    </a>
-                    <p class="flex-grow-1">
-                      <prismic-rich-text  :field="item.card_text"/>
-                    </p>                 
-                  </div>
+                  <p class="flex-grow-1">{{$prismic.richTextAsPlain(item.card_text)}}</p>
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </section>
+    </div>
+  </section>
 </template>
 
 <script>
-
-
 export default {
   props: ["slice"],
-  name: "basic-cards-slice" 
+  name: "basic-cards-slice"
 };
 </script>
