c6a09982a324dc81082d709ecf6d9443f58bb529
HH <hurricane@kotukusystems.com>
2020-04-06 09:29:37 +1200

    Upgrade changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -43,7 +43,8 @@ module.exports = {
 
     '~/plugins/vue-gallery.client.js',
     '~/plugins/google-maps',
-    '~/plugins/vue-good-table' 
+    '~/plugins/vue-good-table',
+    '~/plugins/vue-mailchimp-subscribe'  
   ],
 
   /*
